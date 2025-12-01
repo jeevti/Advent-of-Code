dc43597427266dfd78f5672e14029a7030930770
jeevti <jeevti@gmail.com>
2025-12-01 20:53:51 +0000

    Day 1 Part 1 and 2

--- adventofcodeday1.py
+++ adventofcodeday1.py
@@ -8,15 +8,23 @@ def split_term(s):
 
 for i in data:
     letter, num = split_term(i)
+    if num>99:
+        full = num//100
+        num = num%100
+        zerocounter+=full
     if letter=='R':
+        if (score+num)>99 and (score+num)%100!=0:
+            zerocounter+=1
+            # print("r",num)
         score=(score+num) % 100
         # print(score)
     if letter=='L':
+        if (score-num)<0 and score!=0:
+            zerocounter+=1
+            # print("l",num)
         score=(score-num) %100
         # print(score)
     if score==0:
         zerocounter+=1
-        print(zerocounter)
-
-    
-    
+        # print(zerocounter)
+print(zerocounter)
